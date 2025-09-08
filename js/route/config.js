5c9f7bfc96c5ab49dd82a64def5edad75d1a773b
User <jonathanzmora@gmail.com>
2025-09-08 16:11:23 +0300

    Final Change to routing algorithm

--- js/route/config.js
+++ js/route/config.js
@@ -5,6 +5,6 @@ export const RECENT_LOCATIONS_FRONT_URL = 'https://2v5p2advz3c3iqurwxwz2rsfbm0aw
 export const SAVED_ROUTES_FRONT_URL = 'https://spsslgakqninpdwmeuxbxbpmae0qumqm.lambda-url.us-east-1.on.aws/';
 export const SAVED_ROUTES_URL = 'https://hcyxcqvabf4hwovw3gwcgadiri0icfxi.lambda-url.us-east-1.on.aws/';
 export const ROUTE_CANDIDATES_URL = 'https://f6fja4deomveqvo7yhlzunvxn40ocvbz.lambda-url.us-east-1.on.aws/';
-export const ROUTE_OPTIMIZE_URL   = 'https://smamyxd26mtkpjdzf72k7n2hai0opsys.lambda-url.us-east-1.on.aws/';
+export const ROUTE_OPTIMIZE_URL   = 'https://sv2kkj7lztruxu2ucegpxgtv2a0sebcz.lambda-url.us-east-1.on.aws/';
 export const MAX_CONCURRENCY = 4;
 
